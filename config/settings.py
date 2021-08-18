5f30348ca614cb4fff9b1a3fecdbbf92322b9496
nickmwangemi <nickmwangemi@gmail.com>
2021-08-18 14:52:32 +0300

    Set ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','nicks-simple-reorder-module.herokuapp.com']
 
 
 # Application definition
