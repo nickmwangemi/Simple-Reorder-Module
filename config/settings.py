fb380ac280a6af74e216fcdf5e82c35225767c91
nickmwangemi <nickmwangemi@gmail.com>
2021-08-18 14:43:20 +0300

    Set DEBUG

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', '') != 'False'
+# DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+
 
 ALLOWED_HOSTS = ['*']
 
